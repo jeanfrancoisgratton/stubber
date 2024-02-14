09c24955bb930bc279c55a93aad99ab995520a96
jeanfrancoisgratton <jean-francois@famillegratton.net>
2024-02-14 16:29:31 -0500

    Removed arch variable as we no longer support arm64

--- rpmbuild-deps.sh
+++ rpmbuild-deps.sh
@@ -11,10 +11,9 @@ if [ "$#" -lt 2 ]; then
 fi
 
 export VER=${1}
-export ARCH=${2}
 
 echo "Fetching archive..."
-sudo wget -q https://go.dev/dl/go${VER}.linux-${ARCH}.tar.gz -O /tmp/go.tar.gz -O /opt/go.tar.gz
+sudo wget -q https://go.dev/dl/go${VER}.linux-amd64.tar.gz -O /tmp/go.tar.gz -O /opt/go.tar.gz
 
 echo "Unarchiving..."
 cd /opt ; sudo rm -rf go;sudo tar zxf go.tar.gz; sudo rm -f go.tar.gz
