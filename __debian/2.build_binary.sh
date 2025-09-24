60e4154b299fdbbbe71aa7d2b82c03ae0a61740d
Binary package builder <builder@famillegratton.net>
2025-09-24 02:59:13 -0400

    Package version bump

--- __debian/2.build_binary.sh
+++ __debian/2.build_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PKGDIR=stubber-1.80.00-0_amd64
+PKGDIR=stubber-1.80.01-0_amd64
 
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
 for i in control preinst prerm postinst postrm;do
